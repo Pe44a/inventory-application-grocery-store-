66eded75b81ce1fe927f6706be14135195c2c1b9
Peteris <blankfeldspeteris@gmail.com>
2024-07-02 22:47:40 +0300

    Change rate limit

--- express-inventory-application-grocery-store/app.js
+++ express-inventory-application-grocery-store/app.js
@@ -18,7 +18,7 @@ var app = express();
 // Set up rate limiter: maximum of twenty requests per minute
 const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
-  windowMs: 1 * 60 * 1000, // 1 minute
+  windowMs: 1 * 120 * 1000, // 2 minute
   max: 20,
 });
 // Apply rate limiter to all requests
